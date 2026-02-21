6aa57569135c317000b91cb5a752f19a074e5c15
SEKARr77 <sekar777r@gmail.com>
2026-02-21 14:51:28 +0530

    fixed over all cgpa

--- sem1.js
+++ sem1.js
@@ -9,25 +9,36 @@ const gradePoints = {
 };
 
 function calculateCGPA() {
+
     let table = document.getElementById("gradeTable");
     let totalCredits = 0;
     let totalPoints = 0;
+    let allSelected = true;
 
     for (let i = 1; i < table.rows.length; i++) {
+
         let credits = parseInt(table.rows[i].cells[1].innerText);
         let grade = table.rows[i].cells[2].querySelector("select").value;
 
-        if (grade !== "") {
-            totalCredits += credits;
-            totalPoints += gradePoints[grade] * credits;
+        // If any subject not selected
+        if (grade === "") {
+            allSelected = false;
+            break;
         }
+
+        totalCredits += credits;
+        totalPoints += gradePoints[grade] * credits;
     }
 
-    if (totalCredits === 0) {
-        document.getElementById("result").innerHTML = "CGPA: 0.00";
+    if (!allSelected) {
+        document.getElementById("result").innerHTML =
+            "⚠ Please select grades for all subjects";
         return;
     }
 
     let cgpa = (totalPoints / totalCredits).toFixed(2);
-    document.getElementById("result").innerHTML = "CGPA: " + cgpa;
+
+    document.getElementById("result").innerHTML =
+        "Total Credits: " + totalCredits +
+        "<br>CGPA: " + cgpa;
 }
